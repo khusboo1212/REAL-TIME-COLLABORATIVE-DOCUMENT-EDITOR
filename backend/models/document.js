980d0e337f22616ffe96dd9ee914d35527aaee9d
khusboo1212 <muskan.st.pauls7@gmail.com>
2025-07-24 21:18:54 +0530

    Create document.js

---
+++ backend/models/document.js
@@ -0,0 +1,9 @@
+/ backend/models/Document.js
+const mongoose = require('mongoose');
+
+const DocumentSchema = new mongoose.Schema({
+  _id: String,
+  data: Object,
+});
+
+module.exports = mongoose.model('Document', DocumentSchema);
